e884ee67b60023f42a7f254efde26abacf87d25d
Tarantula471 <singhtushar956@gmail.com>
2023-03-22 21:05:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { Calendar, CalendarOptions } from '@fullcalendar/core';
+import { NgbOffcanvas, OffcanvasDismissReasons, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import * as bootstrap from 'bootstrap';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'OnlineUniversity';
+
+export class AppComponent implements OnInit{
+	title = 'OnlineUniversity';
+
+	ngOnInit(): void {
+		// // alert js
+		// const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+	
+		// const alert = (message : string, type : string) => {
+		//   const wrapper = document.createElement('div')
+		//   wrapper.innerHTML = [
+		// 	`<div class="alert alert-${type} alert-dismissible" role="alert">`,
+		// 	`   <div>${message}</div>`,
+		// 	'   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+		// 	'</div>'
+		//   ].join('')
+	
+		//   alertPlaceholder?.append(wrapper)
+		// }
+	
+		// const alertTrigger = document.getElementById('liveAlertBtn')
+		// if (alertTrigger) {
+		//   alertTrigger.addEventListener('click', () => {
+		// 	alert('Hey, this is a earthquake alert', 'danger')
+		//   })
+		// }
+	
+	
+	
+		// // popover js
+		// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+		// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+		// // SO50234481
+	
+	
+		// // toast js
+		// const toastTrigger = document.getElementById('liveToastBtn')
+		// const toastLiveExample = document.getElementById('liveToast')! // SO63520680
+		// if (toastTrigger) {
+		//   toastTrigger.addEventListener('click', () => {
+		// 	const toast = new bootstrap.Toast(toastLiveExample)
+	
+		// 	toast.show()
+		//   })
+		// }
+	
+		// // tooltip js
+		// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+		// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+	}
+
+	closeResult = '';
+
+	constructor(private offcanvasService: NgbOffcanvas) {}
+
+	open(content: any) {
+		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+	private getDismissReason(reason: any): string {
+		if (reason === OffcanvasDismissReasons.ESC) {
+			return 'by pressing ESC';
+		} else if (reason === OffcanvasDismissReasons.BACKDROP_CLICK) {
+			return 'by clicking on the backdrop';
+		} else {
+			return `with: ${reason}`;
+		}
+	}
+    calendarOptions: CalendarOptions = {
+        initialView: 'dayGridMonth',
+        plugins: [dayGridPlugin,interactionPlugin],
+        weekends: false,
+		events: [
+			{ title: 'Meeting', start: new Date() , end: '2023-03-24'},
+		],
+  	};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,67 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatListModule} from '@angular/material/list';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+	AppComponent,
+	HomeComponent,
+ FooterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+	BrowserModule,
+	AppRoutingModule,
+	MdbAccordionModule,
+	MdbCarouselModule,
+	MdbCheckboxModule,
+	MdbCollapseModule,
+	MdbDropdownModule,
+	MdbFormsModule,
+	MdbModalModule,
+	MdbPopoverModule,
+	MdbRadioModule,
+	MdbRangeModule,
+	MdbRippleModule,
+	MdbScrollspyModule,
+	MdbTabsModule,
+	MdbTooltipModule,
+	MdbValidationModule,
+	BrowserAnimationsModule,
+	MatToolbarModule,
+	MatIconModule,
+	MatSidenavModule,
+	MatNativeDateModule,
+	MatListModule,
+	NgbModule,
+	FullCalendarModule
   ],
+  entryComponents: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
