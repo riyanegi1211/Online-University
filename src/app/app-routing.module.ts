c92f38f4696f3a7379ca26d41d26ce84d302a6a3
riyanegi1211 <riyasnegi12@gmail.com>
2023-03-26 19:31:04 +0530

    Third Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    component: AppComponent,
-    children: [
-      {
-        path: '',
-        component: LoginComponent
-      }
-    ]
-  }
+  { path:" ",component: AppComponent},
+  { path:"Login", component: LoginComponent},
+  { path: 'StudentDashboard', component: StudentDashboardComponent}
 ];
 
 @NgModule({
