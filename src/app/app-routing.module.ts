1a70ce8c8d26d6280eed2896d3caccd205128a21
riyanegi1211 <riyasnegi12@gmail.com>
2023-03-28 16:13:16 +0530

    Course Overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AssessmentComponent } from './assessment/assessment.component';
 import { AnnouncementComponent } from './announcement/announcement.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: "Home",component:HomeComponent},
   {path: "Assessment",component:AssessmentComponent},
   {path: "Announcement",component:AnnouncementComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
@@ -14,18 +15,24 @@ import { MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { AssessmentComponent } from './assessment/assessment.component';
 import { AnnouncementComponent } from './announcement/announcement.component';
+import { AngularFileUploaderModule } from "angular-file-uploader";
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AssessmentComponent,
-    AnnouncementComponent
+    AnnouncementComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFileUploaderModule,
     BrowserAnimationsModule,
     FormsModule,
+    MatExpansionModule,
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
