9be5003acbc457bde6cfb49b2ddbc1fe4a4321ca
Tarantula471 <singhtushar956@gmail.com>
2023-03-28 13:27:32 +0530

    changed frontend

--- demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://example.com")
+            .allowedOrigins("http://localhost:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Authorization", "Content-Type")
             .allowCredentials(true)
