2f95412aa80cf064192231213dd49677efae11e6
Shalabh Agarwal <shalabhagarwal1024@gmail.com>
2023-05-29 13:49:41 +0530

    updated file entity

--- backend/enrollment/src/main/java/com/example/enrollment/model/StudentCourseDataFile.java
+++ backend/enrollment/src/main/java/com/example/enrollment/model/StudentCourseDataFile.java
@@ -1,5 +1,6 @@
 package com.example.enrollment.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,6 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Data
 @Getter
 @Setter
