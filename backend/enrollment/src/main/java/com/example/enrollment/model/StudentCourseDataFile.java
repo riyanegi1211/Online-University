38a6b36d7ac8d96989e7b14b47865bd64175e4da
Shalabh Agarwal <shalabhagarwal1024@gmail.com>
2023-05-29 13:13:46 +0530

    updated file upload service on backend

---
+++ backend/enrollment/src/main/java/com/example/enrollment/model/StudentCourseDataFile.java
@@ -0,0 +1,23 @@
+package com.example.enrollment.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentCourseDataFile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long fileId;
+    long enrollmentId;
+    String fileName;
+}
